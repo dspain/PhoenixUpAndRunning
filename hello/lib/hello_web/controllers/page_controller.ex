c3d63c3f1d295e9c80ae759010cf5497e9f2f638
dspain <dmspain@yahoo.com>
2017-12-26 21:48:36 -0800

    assign a value into the template

--- hello/lib/hello_web/controllers/page_controller.ex
+++ hello/lib/hello_web/controllers/page_controller.ex
@@ -3,8 +3,7 @@ defmodule HelloWeb.PageController do
 
   def index(conn, _params) do
     conn
-    |> put_flash(:info, "Welcome to Phoenix, from flash info!")
-    |> put_flash(:error, "Let's pretend we have an error.")
+    |> assign(:message, "Welcome Back!")
     |> render("index.html")
   end
 
