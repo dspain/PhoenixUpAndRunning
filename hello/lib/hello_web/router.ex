1651ae145789ba01baf9cb1559fbdb64aea9fd6c
dspain <dmspain@yahoo.com>
2017-12-26 21:39:55 -0800

    comment out non-existing pipes/plugs

--- hello/lib/hello_web/router.ex
+++ hello/lib/hello_web/router.ex
@@ -13,10 +13,10 @@ defmodule HelloWeb.Router do
     plug :accepts, ["json"]
   end
 
-  pipeline :review_checks do
-    plug :ensure_authenticated_user
-    plug :ensure_user_owns_review
-  end
+  # pipeline :review_checks do
+  #   plug :ensure_authenticated_user
+  #   plug :ensure_user_owns_review
+  # end
 
   scope "/", HelloWeb do
     pipe_through :browser # Use the default browser stack
@@ -42,7 +42,7 @@ defmodule HelloWeb.Router do
   end
 
   scope "/reviews", HelloWeb do
-    pipe_through :review_checks
+    # pipe_through :review_checks
 
     resources "/", ReviewController
   end
