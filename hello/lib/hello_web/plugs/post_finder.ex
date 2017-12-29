af920718a3f4d916a58ebb33bd5a562d9f37b154
dspain <dmspain@yahoo.com>
2017-12-28 19:02:52 -0800

    comment this out so app will compile

--- hello/lib/hello_web/plugs/post_finder.ex
+++ hello/lib/hello_web/plugs/post_finder.ex
@@ -1,20 +1,20 @@
-defmodule HelloWeb.PostFinder do
-  use Plug
-  import Plug.Conn
-
-  alias Hello.Blog
-
-  def init(opts), do: opts
-
-  def call(conn, _) do
-    case Blog.get_post(conn.params["id"]) do
-      {:ok, post} ->
-        assign(conn, :post, post)
-      {:error, :notfound} ->
-        conn
-        |> send_resp(404, "Not found")
-        |> halt()
-    end
-  end
-
-end
+# defmodule HelloWeb.PostFinder do
+#   use Plug
+#   import Plug.Conn
+#
+#   alias Hello.Blog
+#
+#   def init(opts), do: opts
+#
+#   def call(conn, _) do
+#     case Blog.get_post(conn.params["id"]) do
+#       {:ok, post} ->
+#         assign(conn, :post, post)
+#       {:error, :notfound} ->
+#         conn
+#         |> send_resp(404, "Not found")
+#         |> halt()
+#     end
+#   end
+#
+# end
