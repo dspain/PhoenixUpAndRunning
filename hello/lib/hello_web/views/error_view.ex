bded07e7ad62ea240da64eeece0f701e3a9e1eff
dspain <dmspain@yahoo.com>
2017-12-29 21:54:28 -0800

    show custom 404 page

--- hello/lib/hello_web/views/error_view.ex
+++ hello/lib/hello_web/views/error_view.ex
@@ -1,9 +1,9 @@
 defmodule HelloWeb.ErrorView do
   use HelloWeb, :view
 
-  def render("404.html", _assigns) do
-    "Page not found"
-  end
+  # def render("404.html", _assigns) do
+  #   "Page not found"
+  # end
 
   def render("500.html", _assigns) do
     "Internal server error"
