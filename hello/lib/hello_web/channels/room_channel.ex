e83e73b683e822151c70637a4dc7ab2efe48b7c6
dspain <dmspain@yahoo.com>
2018-01-05 18:47:48 -0800

    add room channel that Presence can communicate on

--- hello/lib/hello_web/channels/room_channel.ex
+++ hello/lib/hello_web/channels/room_channel.ex
@@ -1,15 +1,29 @@
 defmodule HelloWeb.RoomChannel do
-  use Phoenix.Channel
+  use HelloWeb, :channel
 
-  def join("room:lobby", _message, socket) do
-    {:ok, socket}
-  end
-  def join("room:" <> _private_room_id, _params, _socket) do
-    {:error, %{reason: "unauthorized"}}
+  def join("room:lobby", payload, socket) do
+    if authorized?(payload) do
+      {:ok, socket}
+    else
+      {:error, %{reason: "unauthorized"}}
+    end
   end
 
-  def handle_in("new_msg", %{"body" => body}, socket) do
-    broadcast! socket, "new_msg", %{body: body}
+  # Channels can be used in a request/response fashion
+  # by sending replies to requests from the client
+  def handle_in("ping", payload, socket) do
+    {:reply, {:ok, payload}, socket}
+  end
+
+  # It is also common to receive messages from the client and
+  # broadcast to everyone in the current topic (room:lobby).
+  def handle_in("shout", payload, socket) do
+    broadcast socket, "shout", payload
     {:noreply, socket}
+  end
+
+  # Add authorization logic here as required.
+  defp authorized?(_payload) do
+    true
   end
 end
