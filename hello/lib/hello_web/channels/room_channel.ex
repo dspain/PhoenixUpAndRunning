876481f095c1db6be7b67c02b0fb1011b9fbab38
dspain <dmspain@yahoo.com>
2018-01-07 13:31:28 -0800

    update channel to manage the state and presence

--- hello/lib/hello_web/channels/room_channel.ex
+++ hello/lib/hello_web/channels/room_channel.ex
@@ -1,12 +1,18 @@
 defmodule HelloWeb.RoomChannel do
-  use HelloWeb, :channel
+  use Phoenix.Channel
+  alias HelloWeb.Presence
 
-  def join("room:lobby", payload, socket) do
-    if authorized?(payload) do
-      {:ok, socket}
-    else
-      {:error, %{reason: "unauthorized"}}
-    end
+  def join("room:lobby", _params, socket) do
+    send(self(), :after_join)
+    {:ok, socket}
+  end
+
+  def handle_info(:after_join, socket) do
+    push socket, "presence_state", Presence.list(socket)
+    {:ok, _} = Presence.track(socket, socket.assigns.user_id, %{
+      online_at: inspect(System.system_time(:seconds))
+    })
+    {:noreply, socket}
   end
 
   # Channels can be used in a request/response fashion
