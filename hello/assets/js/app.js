718832f08ec9608de9b0aa3518d17bf3c7694eb5
dspain <dmspain@yahoo.com>
2018-01-10 22:18:30 -0800

    get rid of presence stuff

--- hello/assets/js/app.js
+++ hello/assets/js/app.js
@@ -20,36 +20,36 @@ import "phoenix_html"
 
 // import socket from "./socket"
 
-import {Socket, Presence} from "phoenix"
-
-let socket = new Socket("/socket", {
-  params: { user_id: window.location.search.split("=")[1]}
-})
-
-function renderOnlineUsers(presence) {
-  let response = "";
-
-  Presence.list(presences, (id, {metas: [first, ...rest]}) => {
-    let count = rest.length + 1
-    response += `<br>${id} (count: ${count})</br>`
-  });
-
-  document.querySelector("main[role=main]").innerHTML = response;
-}
-
-socket.connect()
-
-let presences = {}
-let channel = socket.channel("room:lobby", {})
-
-channel.on("presence_state", state => {
-  presences = Presence.syncState(presences, state)
-  renderOnlineUsers(presences)
-})
-
-channel.on("presence_diff", diff => {
-  presences = Presence.syncDiff(presences, diff)
-  renderOnlineUsers(presences)
-})
-
-channel.join()
+// import {Socket, Presence} from "phoenix"
+//
+// let socket = new Socket("/socket", {
+//   params: { user_id: window.location.search.split("=")[1]}
+// })
+//
+// function renderOnlineUsers(presence) {
+//   let response = "";
+//
+//   Presence.list(presences, (id, {metas: [first, ...rest]}) => {
+//     let count = rest.length + 1
+//     response += `<br>${id} (count: ${count})</br>`
+//   });
+//
+//   document.querySelector("main[role=main]").innerHTML = response;
+// }
+//
+// socket.connect()
+//
+// let presences = {}
+// let channel = socket.channel("room:lobby", {})
+//
+// channel.on("presence_state", state => {
+//   presences = Presence.syncState(presences, state)
+//   renderOnlineUsers(presences)
+// })
+//
+// channel.on("presence_diff", diff => {
+//   presences = Presence.syncDiff(presences, diff)
+//   renderOnlineUsers(presences)
+// })
+//
+// channel.join()
