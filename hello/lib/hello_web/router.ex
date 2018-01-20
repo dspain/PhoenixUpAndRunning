284739c6c01351e10281d50e62059a5b7717f36e
dspain <dmspain@yahoo.com>
2018-01-19 21:02:44 -0800

    add a function to authenticate user based on session user id

--- hello/lib/hello_web/router.ex
+++ hello/lib/hello_web/router.ex
@@ -26,4 +26,15 @@ defmodule HelloWeb.Router do
     resources "/sessions", SessionController, only: [:new, :create, :delete], singleton: true
   end
 
+  defp authenticate_user(conn, _) do
+    case get_session(conn, :user_id) do
+      nil ->
+        conn
+        |> Phoenix.Controller.put_flash(:error, "Login required")
+        |> Phoenix.Controller.redirect(to: "/")
+        |> halt()
+      user_id ->
+        assign(conn, :current_user, Hello.Accounts.get_user!(user_id))
+    end
+  end
 end
