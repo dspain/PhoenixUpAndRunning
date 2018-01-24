4016f39939883695df30b21763d1421ed36cfc9a
dspain <dmspain@yahoo.com>
2018-01-23 21:36:51 -0800

    username

--- hello/config/test.exs
+++ hello/config/test.exs
@@ -12,7 +12,7 @@ config :logger, level: :warn
 # Configure your database
 config :hello, Hello.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
+  username: "damianspain",
   password: "postgres",
   database: "hello_test",
   hostname: "localhost",
