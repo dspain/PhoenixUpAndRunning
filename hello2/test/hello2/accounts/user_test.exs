0a37e9eabf6b1cd6a8895382e7071cfd2f0f1dfd
dspain <dmspain@yahoo.com>
2018-01-25 22:08:05 -0800

    create user changeset test

---
+++ hello2/test/hello2/accounts/user_test.exs
@@ -0,0 +1,18 @@
+defmodule Hello2.Users.UserTest do
+  use Hello2.DataCase
+
+  alias Hello2.Users.User
+
+  @valid_attrs %{bio: "my life", email: "pat@example.com", name: "Pat Example", number_of_pets: 4}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = User.changeset(%User{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+end
