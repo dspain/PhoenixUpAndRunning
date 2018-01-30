33d29494b5b90a665a816d269864ecd571654e1c
dspain <dmspain@yahoo.com>
2018-01-29 21:04:46 -0800

    create html resource

---
+++ hello2/lib/hello2/accounts/user.ex
@@ -0,0 +1,22 @@
+defmodule Hello2.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Hello2.Accounts.User
+
+
+  schema "users" do
+    field :bio, :string
+    field :email, :string
+    field :name, :string
+    field :number_of_pets, :integer
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%User{} = user, attrs) do
+    user
+    |> cast(attrs, [:name, :email, :bio, :number_of_pets])
+    |> validate_required([:name, :email, :bio, :number_of_pets])
+  end
+end
